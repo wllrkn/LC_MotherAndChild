f93d0f6e2f2e94961847dd03a5688c3c5de16a8a
willl <willl@DESKTOP-9K61CVA>
2025-02-09 17:31:16 +0000

    Add project files.

---
+++ LC_MotherAndChild/Patches/PlayerControllerBPatch.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GameNetcodeStuff;
+using HarmonyLib;
+
+namespace LC_MotherAndChild.Patches
+{
+    [HarmonyPatch(typeof(PlayerControllerB))]
+    internal class PlayerControllerBPatch
+    {
+        // using update as it updates every frame, which will apply our patch
+        [HarmonyPatch("Update")] // as this private, we have to do it this way. If its public, use nameof(PlayerControllerB.Update)
+        [HarmonyPostfix] // This tells us where to put the code, is it pre or post
+        static void infiniteSprintPatch(ref float ___sprintMeter) // this tells us its a reference to something else, accessing the class
+        {
+            ___sprintMeter = 1f; // f defines it as a float, which means here we're just locking it at sprint speed 1
+        }
+    }
+}
+ 
---
+++ LC_MotherAndChild/plugin.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using LC_MotherAndChild.Patches;
+
+namespace LC_MotherAndChild
+{
+    // this bit tells it that its a bepinex plugin
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class MrMiinxxTutorialModBase : BaseUnityPlugin
+    {
+        // define the mods (TD Why Poseiden?)
+        private const string modGUID = "Poseidon.LCTutorialMod";
+        private const string modName = "LC Tutorial Mod";
+        private const string modVersion = "1.0.0.0";
+
+        // define Harmony mod
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        private static MrMiinxxTutorialModBase Instance;
+
+        internal ManualLogSource mls;
+
+        // entry point of the mod
+        void Awake()
+        {
+            if (Instance == null)
+            {
+                // load the instance
+                Instance = this;
+            }
+
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+
+            mls.LogInfo("This is Wills first mod");
+
+            harmony.PatchAll(typeof(MrMiinxxTutorialModBase));
+            harmony.PatchAll(typeof(PlayerControllerBPatch));
+        }
+    }
+}
